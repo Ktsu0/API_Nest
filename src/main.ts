f269113f9044d810c6b39f8056025a3c564d4590
Ktsu0 <gabrielwag971@gmail.com>
2025-10-22 21:30:22 -0300

    introdução OTP

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.enableCors({
-    origin: 'http://localhost:3000', // ou a porta do seu React
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
